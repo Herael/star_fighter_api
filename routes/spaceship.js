a7d9a4329080aff524f4d3613080293bfe9db194
Herael <b.rousval@hotmail.com>
2019-12-23 22:44:04 +0100

    finish struct of spaceship and add /DROP for drop all spaceship database

--- routes/spaceship.js
+++ routes/spaceship.js
@@ -97,6 +97,17 @@ router.post('/getByElement', async function(req, res, next) {
 router.post('/add', async function(req, res){
 
     const name = req.body.name;
+    const model = req.body.model;
+    const manufacturer = req.body.manufacturer;
+    const costInCredits = req.body.costInCredits;
+    const length = req.body.length;
+    const crew = req.body.crew;
+    const passengers = req.body.passengers;
+    const cargoCapacity = req.body.cargoCapacity;
+    const speed = req.body.speed;
+    const spaceshipClass = req.body.spaceshipClass;
+    const pilots = req.body.pilots;
+    const films = req.body.films;
 
     const client = new MongoClient(MONGODB_URI);
     client.connect()
@@ -112,8 +123,18 @@ router.post('/add', async function(req, res){
             } else {
                 const newSpaceship = {
                     name: name,
-                    speed: 1,
-                    createdAt: dateNow(),
+                    model: model,
+                    manufacturer: manufacturer,
+                    costInCredits: costInCredits,
+                    length: length,
+                    crew: crew,
+                    passengers: passengers,
+                    cargoCapacity: cargoCapacity,
+                    speed: speed,
+                    spaceshipClass: spaceshipClass,
+                    pilots: pilots,
+                    films: films,
+                    createdAt: dateNow()
                 };
                 await db.collection(MONGODB_COLLEC).insertOne(newSpaceship);
                 const spaceshipInDb = await db.collection(MONGODB_COLLEC).find({ name: name}).toArray();
@@ -122,7 +143,6 @@ router.post('/add', async function(req, res){
                     error: null,
                     _id: ObjectId(spaceshipInDb[0]._id),
                     name: spaceshipInDb[0].name,
-                    speed: 1,
                     createdAt: spaceshipInDb[0].createdAt
                 });
             }
@@ -132,6 +152,35 @@ router.post('/add', async function(req, res){
             error: error
         });
         client.close();
+    });
+});
+
+
+/**
+ * @DELETE | DELETE all spaceships
+ *
+ * @Route("/DROP")
+ */
+router.delete('/DROP', async function(req, res, next) {
+
+    const client = new MongoClient(MONGODB_URI);
+    client.connect().then(async function(response) {
+        const db = client.db(MONGODB_DBNAME);
+        const spaceships = await db.collection(MONGODB_COLLEC).find().toArray();
+        for (i = 0; i < spaceships.length; i++) {
+            await db.collection(MONGODB_COLLEC).deleteOne(spaceships[i]);
+        }
+        client.close();
+        res.status(200).send({
+            error: null,
+            spaceships: spaceships
+        });
+    }).catch(function (error) {
+        console.log("Error server " + error.stack);
+        res.status(500).send({
+            error: error.stack,
+            spaceships: []
+        });
     });
 });
 
